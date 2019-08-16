9521fb013375a10f564ab0f07116e8af42132353
Vikman Fernandez-Castro <victor@wazuh.com>
2019-08-16 19:45:34 +0200

    Add FIM integrity blockset test to Wazuh DB testsuite

--- integration_tests/wazuh-db/test-wazuh-db.py
+++ integration_tests/wazuh-db/test-wazuh-db.py
@@ -3,7 +3,7 @@
 
 from socket import socket, AF_UNIX, SOCK_STREAM
 from struct import pack, unpack
-from random import choice
+from random import choice, randint, randrange
 from string import ascii_lowercase
 from os import listdir, unlink
 import json
@@ -11,6 +11,32 @@ from testsuite import *
 
 def random_string(length=32):
     return ''.join([choice(ascii_lowercase) for i in range(length)])
+
+
+def random_fim():
+    block0 = randrange(0, 100000)
+    block1 = block0 // 2209
+    block2 = block1 // 47
+
+    return {
+        'file': random_string(),
+        'type': choice(('file', 'registry')),
+        'size': randint(0, 1000000000),
+        'perm': randint(0, 0xFFFFFFFF),
+        'uid': random_string(8),
+        'gid': random_string(8),
+        'md5': random_string(8),
+        'sha1': random_string(8),
+        'uname': random_string(8),
+        'gname': random_string(8),
+        'mtime': randint(0, 1000000000),
+        'inode': randint(0, 1000000000),
+        'sha256': random_string(8),
+        'attrs': randint(0, 1000000000),
+        'symbolic_path': random_string(8),
+        'checksum': random_string(16),
+        'blocks': (block0, block1, block2)
+    }
 
 class Database:
     def __init__(self, id=0):
@@ -46,6 +72,14 @@ class Database:
         self.send("agent {0} syscheck save file {1} {2}".format(self.id, checksum, path))
         return self.recv()
 
+    def save2(self, data):
+        self.send("agent {0} syscheck save2 {1}".format(self.id, json.dumps(data)))
+        return self.recv()
+
+    def make_integrity(self):
+        self.send("agent {0} syscheck make_integrity ".format(self.id))
+        return self.recv()
+
     def remove(self):
         self.send("agent {0} remove".format(self.id))
         return self.recv()
@@ -75,23 +109,60 @@ def test_fim(databases, queries):
             for i in range(1, databases + 1):
                 db.set_id(i)
 
-                for j in range(queries):
+                for _ in range(queries):
                     path = random_string()
                     ans = db.load(path)
 
                     if not ans.startswith("ok"):
-                        raise Exception("Cannot load {0}: {1}", path, ans)
+                        raise Exception("Cannot load {0}: {1}".format(path, ans))
 
                     ans = db.save(path, "0:0:0:0:0:0:0:0:0:0:0:0:0!0:0")
 
                     if ans != "ok":
-                        raise Exception("Cannot save {0}: {1}", path, ans)
+                        raise Exception("Cannot save {0}: {1}".format(path, ans))
 
     except Exception as e:
         print('# {0}'.format(e))
         return False
 
     return True
+
+
+def test_fim_insert(queries):
+    '''Insert files into agent 001 database'''
+
+    try:
+        with Database('001') as db:
+            for _ in range(1, queries):
+                fim = random_fim()
+                ans = db.save2(fim)
+
+                if ans != "ok":
+                    raise Exception("Cannot save {0}: {1}".format(fim['file'], ans))
+
+    except Exception as e:
+        print('# {0}'.format(e))
+        return False
+
+    return True
+
+
+def test_fim_make_integrity():
+    '''Make a FIM database integrity blockset'''
+
+    try:
+        with Database('001') as db:
+            ans = db.make_integrity()
+
+            if not ans.startswith("ok"):
+                raise Exception("Cannot make integrity: {0}".format(ans))
+
+    except Exception as e:
+        print('# {0}'.format(e))
+        return False
+
+    return True
+
 
 def test_remove_individual(n):
     '''Remove database files'''
@@ -103,7 +174,7 @@ def test_remove_individual(n):
                 ans = db.remove()
 
                 if ans != ("ok"):
-                    raise Exception("Cannot remove {0}: {1}", path, ans)
+                    raise Exception("Cannot remove {0}: {1}".format(i, ans))
 
     except Exception as e:
         print('# {0}'.format(e))
@@ -119,7 +190,7 @@ def test_remove_multiple(n):
             ans = db.remove_multiple([str(i) for i in range(1, n + 1)])
 
             if not ans.startswith("ok "):
-                raise Exception("Cannot remove: {0}", ans)
+                raise Exception("Cannot remove: {0}".format(ans))
 
             json.loads(ans[3:])
 
@@ -149,6 +220,8 @@ if __name__ == "__main__":
     test.append("Remove 1000 databases individually", test_remove_individual(1000))
     test.append("Remove 1000 databases", test_remove_multiple(1000))
     test.append("Remove 5000 databases", test_remove_multiple(5000), expected=False)
+    test.append("Insert 1000 FIM files", test_fim_insert(1000))
+    test.append("Create a FIM database integrity blockset", test_fim_make_integrity())
 
     print(test)
     teardown()
