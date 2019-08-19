302ebae0f9ada8d3f60d3b35a75032140804ab3f
Vikman Fernandez-Castro <victor@wazuh.com>
2019-08-19 12:56:05 +0200

    Add testsuite library option to print results immediately

--- integration_tests/wazuh-db/testsuite.py
+++ integration_tests/wazuh-db/testsuite.py
@@ -11,16 +11,21 @@ def get_directory():
     raise Exception("No such directory configuration in the init file")
 
 class TestSuite:
-    def __init__(self):
+    def __init__(self, autoprint=True):
         self.tests = []
+        self.autoprint = autoprint
 
     def append(self, title, result, expected=True):
         self.tests.append((title, result, expected))
 
+        if self.autoprint:
+            print('{0} - {1}{2}'.format('ok' if result else 'not ok', title, '' if expected else ' # TODO'))
+
     def __str__(self):
         output = '1..{0}'.format(len(self.tests))
 
-        for t in self.tests:
-            output += '\n{0} - {1}{2}'.format('ok' if t[1] else 'not ok', t[0], '' if t[2] else ' # TODO')
+        if not self.autoprint:
+            for t in self.tests:
+                output += '\n{0} - {1}{2}'.format('ok' if t[1] else 'not ok', t[0], '' if t[2] else ' # TODO')
 
         return output
