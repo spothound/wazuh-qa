2624f5305463c81ec5faba6aaa1ff9a87cf5b018
Víctor Rebollo <victorrebollop@gmail.com>
2021-03-15 17:16:55 +0100

    Fixed test

--- deps/wazuh_testing/wazuh_testing/logcollector.py
+++ deps/wazuh_testing/wazuh_testing/logcollector.py
@@ -1,5 +1,14 @@
 from wazuh_testing.tools import monitoring
 
 def callback_analyzing_file(file):
-    msg = fr"Info: \(\d+\): Analyzing file: '{file}'."
+    msg = fr"INFO: \(\d+\): Analyzing file: '{file}'."
+    return monitoring.make_callback(pattern=msg, prefix=monitoring.LOG_COLLECTOR_DETECTOR_PREFIX)
+
+
+def callback_monitoring_command(log_format, command):
+    msg = fr"INFO: Monitoring output of {command}\(\d+\): {log_format}"
+    return monitoring.make_callback(pattern=msg, prefix=monitoring.LOG_COLLECTOR_DETECTOR_PREFIX)
+
+def callback_monitoring_djb_multilog(program_name, multilog_file):
+    msg = fr"INFO: Using program name '{program_name}' for DJB multilog file: '{multilog_file}'."
     return monitoring.make_callback(pattern=msg, prefix=monitoring.LOG_COLLECTOR_DETECTOR_PREFIX)
--- tests/integration/test_logcollector/test_configuration/test_configuration_log_format.py
+++ tests/integration/test_logcollector/test_configuration/test_configuration_log_format.py
@@ -17,38 +17,44 @@ test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data
 configurations_path = os.path.join(test_data_path, 'wazuh_basic_configuration.yaml')
 
 parameters = [
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'syslog'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'json'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'snort-full'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'eventlog'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'eventchannel'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'mysql_log'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'postgresql_log'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'nmapg'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'iis'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'command'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'full_command'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'djb-multilog'},
-    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'multi-line'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'syslog', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'json', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'snort-full', 'COMMAND': 'example-command'},
+    {'LOCATION': 'Security', 'LOG_FORMAT': 'eventlog', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'eventchannel', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'mysql_log', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'postgresql_log', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'nmapg', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'iis', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'command', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'command', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'full_command', 'COMMAND': 'example-command'},
+    {'LOCATION': '/var/log/testing/current', 'LOG_FORMAT': 'djb-multilog', 'COMMAND': 'example-command'},
+    {'LOCATION': '/var/log/program_name_testing/current', 'LOG_FORMAT': 'djb-multilog', 'COMMAND': 'example-command'},
+    {'LOCATION': '/var/log/testing/current', 'LOG_FORMAT': 'djb-multilog', 'COMMAND': 'example-command'},
+    {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'multi-line:3'},
     {'LOCATION': '/tmp/test.txt', 'LOG_FORMAT': 'invalid'},
 
 ]
 
 metadata = [
-    {'location': '/tmp/test.txt', 'log_format': 'syslog', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'json', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'snort-full', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'eventlog', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'eventchannel', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'mysql_log', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'postgresql_log', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'nmapg', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'iis', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'command', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'full_command', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'djb-multilog', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'multi-line', 'valid': True},
-    {'location': '/tmp/test.txt', 'log_format': 'invalid', 'valid': False},
+    {'location': '/tmp/test.txt', 'log_format': 'syslog', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'json', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'snort-full', 'command': 'example-command', 'valid_value': True},
+    {'location': 'Security', 'log_format': 'eventlog', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'eventchannel', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'mysql_log', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'postgresql_log', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'nmapg', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'iis', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'command', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': '', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'full_command', 'command': 'example-command', 'valid_value': True},
+    {'location': '/var/log/testing/current', 'log_format': 'djb-multilog', 'command': 'example-command', 'valid_value': True},
+    {'location': '/var/log/program_name_testing/current', 'log_format': 'djb-multilog', 'command': 'example-command', 'valid_value': True},
+    {'location': '/var/log/testing/current', 'log_format': 'djb-multilog', 'command': 'example-command','valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'multi-line:3', 'command': 'example-command', 'valid_value': True},
+    {'location': '/tmp/test.txt', 'log_format': 'invalid', 'command': 'example-command', 'valid_value': False}
 ]
 
 
@@ -57,6 +63,7 @@ configurations = load_wazuh_configurations(configurations_path, "test_configurat
                                            metadata=metadata)
 configuration_ids = [f"{x['LOCATION'], x['LOG_FORMAT']}" for x in parameters]
 
+log_format_not_print_analyzing_info = ['command', 'full_command', 'eventlog', 'eventchannel']
 
 # fixtures
 @pytest.fixture(scope="module", params=configurations, ids=configuration_ids)
@@ -69,15 +76,27 @@ def test_log_format_valid(get_configuration, configure_environment, restart_logc
     """
     """
     cfg = get_configuration['metadata']
-    if not cfg['valid']:
+    if not cfg['valid_value']:
         pytest.skip('Invalid values provided')
 
-    log_callback = logcollector.callback_analyzing_file('log_format')
-    wazuh_log_monitor.start(timeout=5, callback=log_callback,
-                            error_message="The expected error output has not been produced")
+    if cfg['log_format'] not in log_format_not_print_analyzing_info :
 
+        log_callback = logcollector.callback_analyzing_file(cfg['location'])
+        wazuh_log_monitor.start(timeout=5, callback=log_callback,
+                                error_message="The expected error output has not been produced")
+    elif 'command' in cfg['log_format']
+
+        log_callback = logcollector.callback_monitoring_command(cfg['location'])
+        wazuh_log_monitor.start(timeout=5, callback=log_callback,
+                                error_message="The expected error output has not been produced")
+
+    elif cfg['log_format'] == 'djb-multilog'
+
+        log_callback = logcollector.callback_monitoring_djb_multilog(cfg['location'])
+        wazuh_log_monitor.start(timeout=5, callback=log_callback,
+                                error_message="The expected error output has not been produced")
+        
     # Check API response and log format analysing file
-    assert 1
 
 
 def test_log_format_invalid(get_configuration, configure_environment, restart_logcollector):
@@ -85,7 +104,7 @@ def test_log_format_invalid(get_configuration, configure_environment, restart_lo
     """
     cfg = get_configuration['metadata']
 
-    if cfg['valid']:
+    if cfg['valid_value']:
         pytest.skip('Valid values provided')
 
     log_callback = gc.callback_invalid_value('log_format', cfg['log_format'], LOG_COLLECTOR_DETECTOR_PREFIX)
@@ -100,3 +119,32 @@ def test_log_format_invalid(get_configuration, configure_environment, restart_lo
     wazuh_log_monitor.start(timeout=5, callback=log_callback,
                             error_message="The expected error output has not been produced")
 
+
+
+"""
+
+FAILED test_logcollector/test_configuration/test_configuration_log_format.py::test_log_format_valid[('Securifdhsakfjasty', 'eventlog')]
+FAILED test_logcollector/test_configuration/test_configuration_log_format.py::test_log_format_valid[('/tmp/test.txt', 'eventchannel')]
+FAILED test_logcollector/test_configuration/test_configuration_log_format.py::test_log_format_valid[('/tmp/test.txt', 'command')]
+FAILED test_logcollector/test_configuration/test_configuration_log_format.py::test_log_format_valid[('/tmp/test.txt', 'full_command')]
+FAILED test_logcollector/test_configuration/test_configuration_log_format.py::test_log_format_valid[('/tmp/test.txt', 'djb-multilog')]
+
+
+Command
+
+2021/03/15 15:32:17 wazuh-logcollector: INFO: Monitoring output of command(360): command
+
+Full command
+2021/03/15 15:31:39 wazuh-logcollector: INFO: Monitoring output of command(360): full_command
+
+
+
+djb-multilog
+2021/03/15 15:34:59 wazuh-logcollector: INFO: Using program name 'testing' for DJB multilog file: '/var/log/testing/current'.
+
+
+
+
+Not show log: evenchannel, eventlog
+
+"""
