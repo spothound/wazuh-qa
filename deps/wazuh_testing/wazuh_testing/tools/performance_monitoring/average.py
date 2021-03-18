cdf8702c71843fe8edbb186679f4ad210675fd49
jmv74211 <jonathan.martin@wazuh.com>
2021-03-18 13:49:04 +0100

    Update average script to eliminate possible outliers #1134

--- deps/wazuh_testing/wazuh_testing/tools/performance_monitoring/average.py
+++ deps/wazuh_testing/wazuh_testing/tools/performance_monitoring/average.py
@@ -10,16 +10,27 @@ if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
 
     arg_parser.add_argument('-f', metavar=('<file_path>'), type=str, required=True, help='File path to read')
+    arg_parser.add_argument('-t', metavar=('<threshold>'), required=False, type=int,
+                            help='Lower limit from the data to be averaged', default=1000)
     arg_parser.add_argument('-w', action='store_true', required=False, help='Write data to file')
     arg_parser.add_argument('-o', metavar=('<output_file_data_name>'),type=str, required=False,
                             help='Output file data name')
 
     script_parameters = arg_parser.parse_args()
 
-    file = script_parameters.f
+    cwd = os.getcwd()
+
+    file = os.path.join(cwd, script_parameters.f)
+    lower_limit = script_parameters.t
+
     output_file_name = script_parameters.o
 
     df = pd.read_csv(file, sep=",")
+    df.columns = df.columns.str.strip()
+
+    # Eliminates manager rows that do not exceed the established limit.
+    if 'events_edps' in df.columns:
+        df = df[df.events_edps > lower_limit]
 
     mean = df.mean().astype(int)
 
