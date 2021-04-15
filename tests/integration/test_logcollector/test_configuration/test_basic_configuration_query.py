5585aa9a3e7e931ee7f54fa7ee9f81f1a8f87080
Víctor Rebollo <victorrebollop@gmail.com>
2021-04-15 16:09:21 +0200

    Testing

--- tests/integration/test_logcollector/test_configuration/test_basic_configuration_query.py
+++ tests/integration/test_logcollector/test_configuration/test_basic_configuration_query.py
@@ -6,6 +6,7 @@ import os
 import pytest
 import wazuh_testing.api as api
 import wazuh_testing.logcollector as logcollector
+from wazuh_testing.tools import get_service
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 import sys
 from wazuh_testing.tools import get_service
@@ -19,6 +20,14 @@ pytestmark = pytest.mark.tier(level=0)
 # Configuration
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_basic_configuration.yaml')
+
+wazuh_component = get_service()
+
+if wazuh_component == 'wazuh-manager':
+    prefix = LOG_COLLECTOR_DETECTOR_PREFIX
+else:
+    prefix = AGENT_DETECTOR_PREFIX
+
 
 
 parameters = [
@@ -73,7 +82,7 @@ def check_configuration_query_valid(cfg):
 def check_configuration_query_invalid():
     """
     """
-    log_callback = logcollector.callback_query_bad_format('SECURITY')
+    log_callback = logcollector.callback_query_bad_format('SECURITY', prefix=prefix)
     wazuh_log_monitor.start(timeout=5, callback=log_callback,
                             error_message="The expected error output has not been produced")
 
@@ -92,5 +101,5 @@ def test_configuration_query(get_configuration, configure_environment, restart_l
     if cfg['valid_value']:
         check_configuration_query_valid(cfg)
     else:
-        check_configuration_query_invalid(cfg)
+        check_configuration_query_invalid()
 
